94072dd5b40f0e1465a96c0fca60e213da8d3d83
Rajveer Singh <777rajveersingh@gmail.com>
2025-11-19 15:22:31 +0530

    all set

--- src/app.js
+++ src/app.js
@@ -4,16 +4,15 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-
 app.use(
     cors({
         origin: process.env.CORS_ORIGIN,
-        credentials: true, // read express docs
+        credentials: true,
     })
 );
 
 app.get("/", (req, res) => {
-  res.send("Streamify backend is running");
+    res.send("Streamify backend is running");
 });
 
 app.use(express.json({ limit: "16kb" }));
@@ -24,13 +23,7 @@ app.use(cookieParser());
 // routes import
 import userRouter from "./routes/user.routes.js";
 
-// routes decleration
-// app.use("/users", userRouter)
-// so now this users has become prefix for our url such as
-// https://localhost:8000/users/ now here controll will go to userRouter
-// but in standard practice we write as:
+// routes declaration
 app.use("/api/v1/users", userRouter);
-// now the url has become https://localhost:8000/api/v1/users/(here will be register or login .....)
 
-export { app };
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,56 +1,16 @@
-// require('dotenv').config({path: './env'})
-import dotenv from 'dotenv'
-
-// import mongoose from 'mongoose'
-// import { DB_NAME } from './constants.js';
+import dotenv from 'dotenv';
 import connectDB from './db/index.js';
-import { app } from './app.js';
+import app from './app.js';
 
-// import express from 'express'
-// const app = express()
-
-dotenv.config({
-    path: './.env'
-})
-
-
-
+dotenv.config({ path: './.env' });
 
 connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running at port: ${process.env.PORT}`)
-    })
-})
-.catch((err) => {
-    console.log("MONGODB connection failed !!! ", err)
-})
-
-
-
-
-
-
-
-
-
-// 1st approach to connect mongodb(using IIFE)
-/*
-;( async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error", (error) => {
-            console.error("ERR: ", error)
-            throw error
-        })
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`)
-        })
-    } catch (error) {
-        console.error("ERROR: ", error)
-        throw error
-    }
-})()
-
-*/
+  .then(() => {
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => {
+      console.log(`Local server running at port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed!", err);
+  });
