a923674e59e31c79a612f39e01e0733bcdf34da9
Rajveer Singh <777rajveersingh@gmail.com>
2025-11-19 14:54:03 +0530

    add slash route

--- src/app.js
+++ src/app.js
@@ -1,31 +1,36 @@
 import express from "express";
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true // read express docs
-}))
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true, // read express docs
+    })
+);
 
+app.get("/", (req, res) => {
+  res.send("Streamify backend is running");
+});
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 // routes import
-import userRouter from './routes/user.routes.js'
-
+import userRouter from "./routes/user.routes.js";
 
 // routes decleration
 // app.use("/users", userRouter)
 // so now this users has become prefix for our url such as
 // https://localhost:8000/users/ now here controll will go to userRouter
 // but in standard practice we write as:
-app.use("/api/v1/users", userRouter)
+app.use("/api/v1/users", userRouter);
 // now the url has become https://localhost:8000/api/v1/users/(here will be register or login .....)
 
-export { app }
-export default app
+export { app };
+export default app;
