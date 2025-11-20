a0ced5f0362229359bc8cd559193d15c6f7a70f7
Rajveer Singh <777rajveersingh@gmail.com>
2025-11-20 12:43:15 +0530

    fix small bug

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,5 +1,5 @@
 import asyncHandler from "../utils/asyncHandler.js";
-import ApiError from "../utils/apiError.js";
+import ApiError from "../utils/ApiError.js";
 import { Video } from "../models/video.model.js";
 import { User } from "../models/user.model.js";
 import { Comment } from "../models/comment.model.js";
